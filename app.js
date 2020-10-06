1c95901b90b44bd50499f909dafa06dc7973e55c
ravielze <ravielz20@gmail.com>
2020-10-06 23:31:46 +0700

    Remove validator for app fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
-let expressValidator = require('express-validator');
 require('dotenv').config()
 
 let app = express();
--- validator/ceritaV.js
+++ validator/ceritaV.js
@@ -1,17 +1,1 @@
 const Cerita = require('../models/ceritaModel');
-
-exports.createCerita = (req, res, next){
-    const {nama, nim, jurusan, angkatan, line_id} = req.body;
-    if (userName && email &&  isValidEmail(email)) { 
-      
-      // isValidEmail is some custom email function to validate email which you might need write on your own or use npm module
-      User.create({
-        userName,
-        email,
-        phone,
-        status,   
-      })
-      .then(user => res.json(user))
-      .catch(next)
-    }
-}
