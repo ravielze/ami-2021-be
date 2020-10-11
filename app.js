8b22bd662e6fcba7eee394c2c528ee657962476b
ravielze <ravielz20@gmail.com>
2020-10-11 20:01:17 +0700

    Try to fix Cors

--- app.js
+++ app.js
@@ -11,21 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(helmet());
 app.disable('x-powered-by');
-
-var allowedOrigins = process.env.ORIGINS;
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 mongoose.connect(process.env.DB || 'mongodb://localhost/ami', { useNewUrlParser: true, useUnifiedTopology: true});
