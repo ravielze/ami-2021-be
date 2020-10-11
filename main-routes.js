4c727eb5b3be36f49147b64d75902b63b6e051b8
ravielze <ravielz20@gmail.com>
2020-10-11 20:26:38 +0700

    Add rate limiter

--- main-routes.js
+++ main-routes.js
@@ -1,13 +1,21 @@
 let router = require('express').Router();
+const rateLimit = require("express-rate-limit");
 let fj = require('./data/fakultasjurusan');
 let ceritaController = require('./controller/ceritaController');
 const { validateCerita } = require('./validator/ceritaV');
+
+const postCeritaLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    max: 3, // start blocking after 5 requests
+    message:
+      "Too many request created from this IP, please try again after an hour"
+  });
 
 router.get('/fakultas', function (req, res) {
     res.end(JSON.stringify({data: fj.jurusan}));
 });
 
 router.route('/cerita').get(ceritaController.index);
-router.route('/cerita').post(validateCerita, ceritaController.new);
+router.route('/cerita').post(validateCerita, postCeritaLimiter, ceritaController.new);
 
 module.exports = router;
