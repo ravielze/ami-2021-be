72cab2dcb1ad91e86e85d739fb076a9fa724fc62
ravielze <ravielz20@gmail.com>
2020-10-11 12:53:43 +0700

    Add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let helmet = require('helmet');
+let cors = require('cors');
 require('dotenv').config()
 
 let app = express();
@@ -10,6 +11,21 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(helmet());
 app.disable('x-powered-by');
+
+var allowedOrigins = process.env.ORIGINS;
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(bodyParser.json());
 mongoose.connect(process.env.DB || 'mongodb://localhost/ami', { useNewUrlParser: true, useUnifiedTopology: true});
@@ -24,6 +40,6 @@ let apiR = require("./main-routes");
 app.use('/api', apiR);
 
 var port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', function () {
+app.listen(port, function () {
      console.log(">> Running Application on port " + port);
 });
