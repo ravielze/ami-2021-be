cb575c5383540adbc7feffcfc284db1d57211c07
ravielze <ravielz20@gmail.com>
2020-10-11 20:14:52 +0700

    Change singkatan jurusan -> jurusan full name

--- data/fakultasjurusan.js
+++ data/fakultasjurusan.js
@@ -1,16 +1,85 @@
 exports.jurusan = {
-    'FITB': ['TPB', 'ME', 'OS', 'GD', 'GL'],
-    'FMIPA': ['TPB', 'AK', 'AS', 'FI', 'KI', 'MA'],
-    'FSRD': ['TPB', 'DI', 'DKV', 'DP', 'KR', 'SR'],
-    'FTMD': ['TPB', 'AE', 'MT', 'MS'],
-    'FTTM': ['TPB', 'TG', 'MG', 'TM', 'TA'],
-    'FTSL': ['TPB', 'IL', 'SA', 'KL', 'TL', 'SI'],
-    'FTI': ['TPB', 'MR', 'TB', 'TF', 'TI', 'TK', 'PG'],
-    'SAPPK': ['TPB', 'AR', 'PWK'],
-    'SBM': ['TPB', 'MK', 'MB'],
-    'SF': ['TPB', 'FKK', 'FA'],
-    'SITH': ['TPB', 'BI', 'BM', 'BE', 'BA', 'BW', 'BP'],
-    'STEI': ['TPB', 'II', 'EB', 'EL', 'IF', 'ET', 'EP']
+    FITB: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK GEOLOGI",
+        "TEKNIK GEODESI DAN GEOMATIKA",
+        "METEOROLOGI",
+        "OSEANOGRAFI",
+      ],
+      FMIPA: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "MATEMATIKA",
+        "FISIKA",
+        "ASTRONOMI",
+        "KIMIA",
+        "AKTUARIA"
+        ],
+      FSRD: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "SENI RUPA",
+        "KRIA",
+        "DESAIN INTERIOR",
+        "DESAIN KOMUNIKASI VISUAL",
+        "DESAIN PRODUK",
+      ],
+      FTI: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK KIMIA",
+        "TEKNIK FISIKA",
+        "TEKNIK INDUSTRI",
+        "MANAJEMEN REKAYASA INDUSTRI",
+        "TEKNIK PANGAN",
+        "TEKNIK BIOENERGI DAN KEMURDI",
+      ],
+      FTMD: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK MESIN", 
+        "TEKNIK DIRGANTARA", 
+        "TEKNIK MATERIAL"],
+      FTTM: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK PERTAMBANGAN",
+        "TEKNIK PERRMINYAKAN",
+        "TEKNIK GEOFISIKA",
+        "TEKNIK METALURGI",
+      ],
+      FTSL: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK SIPIL",
+        "TEKNIK LINGKUNGAN",
+        "TEKNIK KELAUTAN",
+        "TEKNIK DAN PENGELOLAAN SUMBER DAYA AIR",
+        "REKAYASA INFRASTRUKTUR LINGKUNGAN",
+      ],
+      SAPPK: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "ARSITEKTUR",
+        "PERENCANAAN WILAYAH DAN KOTA"],
+      SBM: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "MANAJEMEN",
+        "KEWIRAUSAHAAN"],
+      SF:[
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "Sains dan Teknologi Farmasi",
+        "Farmasi Klinik dan Komunitas"
+      ],
+      SITH: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "BIOLOGI",
+        "MIKROBIOLOGI",
+        "REKAYASA HAYATI",
+        "REKAYASA PERTANIAN",
+        "REKAYASA KEHUTANAN",
+        "TEKNOLOGI PASCA PANEN",
+      ],
+      STEI: [
+        "TAHAP PERSIAPAN BERSAMA (TPB)",
+        "TEKNIK ELEKTRO",
+        "TEKNIK INFORMATIKA",
+        "TEKNIK TENAGA LISTRIK",
+        "TEKNIK TELEKOMUNIKASI",
+        "SISTEM DAN TEKNOLOGI INFORMASI",
+        "TEKNIK BIOMEDIS",
+      ]
 }
-
-exports.alljurusan = ['TPB', 'ME', 'OS', 'GD', 'GL', 'AK', 'AS', 'FI', 'KI', 'MA', 'DI', 'DKV', 'DP', 'KR', 'SR', 'AE', 'MT', 'MS', 'TG', 'MG', 'TM', 'TA', 'IL', 'SA', 'KL', 'TL', 'SI', 'MR', 'TB', 'TF', 'TI', 'TK', 'PG', 'AR', 'PWK', 'MK', 'MB', 'FKK', 'FA', 'BI', 'BM', 'BE', 'BA', 'BW', 'BP', 'II', 'EB', 'EL', 'IF', 'ET', 'EP']
