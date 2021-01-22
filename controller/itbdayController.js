181a352909531c49d079763b7ac92d1ba89ddd6f
ravielze <ravielz20@gmail.com>
2021-01-22 20:15:26 +0700

    add meet

--- controller/itbdayController.js
+++ controller/itbdayController.js
@@ -3,11 +3,13 @@ exports.index = function (req, res) {
     const {code} = req.body;
     if (code == "LET5EK5PL0R3Y0U12DR34M"){
         res.status(200).json({
-            status: "success"
+            status: "success",
+            meet: "https://meet.google.com/ggx-mrgx-qpu"
         });
     } else {
         res.status(400).json({
-            status: "error"
+            status: "error",
+            meet: "???"
         });
     }
 };
